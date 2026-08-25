dc2268a80bfee104547d696aa63b30177428df99
WXH-seu <3526912140@qq.com>
2026-08-25 14:26:21 +0800

    feat: add academic management Swing client

--- vcampus-client/src/main/java/edu/seu/vcampus/client/ui/MainFrame.java
+++ vcampus-client/src/main/java/edu/seu/vcampus/client/ui/MainFrame.java
@@ -1,6 +1,7 @@
 package edu.seu.vcampus.client.ui;
 
 import edu.seu.vcampus.client.network.ClientConnection;
+import edu.seu.vcampus.client.service.AcademicClientService;
 import edu.seu.vcampus.common.dto.LoginResponse;
 import edu.seu.vcampus.common.enums.Operation;
 import edu.seu.vcampus.common.message.RequestMessage;
@@ -60,8 +61,9 @@ public final class MainFrame extends JFrame {
 
         cards.add(new ModulePanel("欢迎使用虚拟校园",
                 "统一入口已经连接到 Java 8 C/S 服务端。"), CARD_NAMES[0]);
-        cards.add(new ModulePanel("学籍管理",
-                "学生、教师、班级与院系信息管理。"), CARD_NAMES[1]);
+        cards.add(new AcademicManagementPanel(
+                new AcademicClientService(connection, session.getSessionToken()),
+                session.getRole()), CARD_NAMES[1]);
         cards.add(new ModulePanel("选课系统",
                 "课程查询、选课、退课与冲突校验。"), CARD_NAMES[2]);
         cards.add(new ModulePanel("图书馆",
